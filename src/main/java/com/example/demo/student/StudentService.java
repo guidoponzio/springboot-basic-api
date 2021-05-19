09f0203a56af7d06d8ea224530eafb363ac311f4
Guido Ponzio <guidonicolasponzio@gmail.com>
2021-05-19 20:17:35 -0300

    Agregado Método PUT. Fix: el commit anterior fue el agregado del método DELETE

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -37,5 +39,28 @@ public class StudentService {
            throw new IllegalStateException("Estudiante con ID " + studentId + " no existe.");
        }
        studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email){
+        Student student = studentRepository.findById(studentId).orElseThrow( () -> new IllegalStateException(
+                "Estudiante con el ID " + studentId + " no existe."
+        ) );
+
+        if(name != null &&
+            name.length() > 0 &&
+            !Objects.equals(student.getName(), name)){
+            student.setName(name);
+        }
+
+        if(email != null &&
+                email.length() > 0 &&
+                !Objects.equals(student.getEmail(), email)){
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if(studentOptional.isPresent()){
+                throw new IllegalStateException("Email ya existente");
+            }
+            student.setEmail(email);
+        }
     }
 }
